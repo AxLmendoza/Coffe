be6c6c66abb2f9214241230717d4db3c07c17f35
Axl Mendoza <axlmendoza02@gmail.com>
2025-02-08 23:37:09 -0600

    ejemplo.js

---
+++ js.js
@@ -0,0 +1,9 @@
+// Función para mostrar un mensaje de bienvenida
+function mostrarMensajeBienvenida() {
+    alert("¡Bienvenido a Blog DeCafé!");
+}
+
+// Llamar a la función cuando la página se haya cargado completamente
+window.onload = function() {
+    mostrarMensajeBienvenida();
+};
